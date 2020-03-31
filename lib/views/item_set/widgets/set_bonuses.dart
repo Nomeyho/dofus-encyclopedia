c3962ba9413ddfe8a523c2e1cca497e50226d276
Stéphane Vanberg <vanbergstephane@gmail.com>
2020-03-31 21:34:30 +0200

    Improve set bonus animation

--- lib/views/item_set/widgets/set_bonuses.dart
+++ lib/views/item_set/widgets/set_bonuses.dart
@@ -3,18 +3,24 @@ import 'package:dofus_items/app_theme.dart';
 import 'package:dofus_items/generated/i18n.dart';
 import 'package:dofus_items/widgets/bonus_characteristics.dart';
 import 'package:dofus_items/widgets/bonus_others.dart';
-import 'package:dofus_items/widgets/fade_in.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class SetBonuses extends StatelessWidget {
+class SetBonuses extends StatefulWidget {
+  @override
+  _SetBonusesState createState() => _SetBonusesState();
+}
+
+class _SetBonusesState extends State<SetBonuses>
+    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     final state = Provider.of<AppState>(context);
     final bonuses = state.selectedSet.bonuses[state.selectedBonusIndex];
 
-    return FadeIn(
-      duration: Duration(milliseconds: 700),
+    return AnimatedSize(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
       child: (bonuses.characteristicBonuses.isEmpty &&
               bonuses.otherBonuses.isEmpty)
           ? Text(
