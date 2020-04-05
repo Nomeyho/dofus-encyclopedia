549a981e156c1500bec581743197c944e8678ace
Stéphane Vanberg <vanbergstephane@gmail.com>
2020-04-05 18:38:07 +0200

    Prepare release on Play Store

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,8 +1,5 @@
-import 'package:dofus_items/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(App());
-  });
+  testWidgets('smoke test', (WidgetTester tester) {});
 }
