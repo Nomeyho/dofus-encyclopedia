e76e8f8c82fb76a57a5b6b11810afe17b8da9d8c
Stéphane Vanberg <vanbergstephane@gmail.com>
2020-04-05 11:48:58 +0200

    Reorganize project

--- lib/domain/other_bonus.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dofus_items/domain/translation.dart';
-
-class OtherBonus {
-  final Translation description;
-
-  const OtherBonus(
-    this.description,
-  );
-}
--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dofus_items/app.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(App());
-  });
-}
