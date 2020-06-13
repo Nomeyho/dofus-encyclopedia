2593bb64ad78669c0a6c0aad2bd91e8b8c237a59
Stéphane Vanberg <vanbergstephane@gmail.com>
2020-06-13 21:30:42 +0200

    "Apply" filter

---
+++ mobile/lib/views/items_filter/items_filter_modal.dart
@@ -0,0 +1,53 @@
+import 'package:dofus_items/app_state.dart';
+import 'package:dofus_items/app_theme.dart';
+import 'package:dofus_items/views/items_filter/widgets/characteristics_filter.dart';
+import 'package:dofus_items/views/items_filter/widgets/filter_title.dart';
+import 'package:dofus_items/views/items_filter/widgets/level_filter.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ItemsFilterModal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /// Make a copy of the filters so that it can be freely modified
+    /// in this view without affecting the search results
+    final state = Provider.of<AppState>(context, listen: false);
+
+    return ListenableProvider(
+      create: (_) => state.itemFilter.clone(),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.75,
+        color: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppTheme.surface,
+            borderRadius: const BorderRadius.only(
+              topLeft: const Radius.circular(24.0),
+              topRight: const Radius.circular(24.0),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 24, left: 32, right: 32),
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FilterTitle(),
+                  Divider(),
+                  Expanded(
+                    child: ListView(
+                      children: <Widget>[
+                        LevelFilter(),
+                        CharacteristicsFilter(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- mobile/lib/views/items_filter/widgets/characteristics_filter.dart
+++ mobile/lib/views/items_filter/widgets/characteristics_filter.dart
@@ -1,19 +1,23 @@
 import 'package:dofus_items/app_theme.dart';
 import 'package:dofus_items/domain/characteristic.dart';
+import 'package:dofus_items/domain/item_filter.dart';
 import 'package:dofus_items/generated/i18n.dart';
 import 'package:dofus_items/views/items_filter/widgets/characteristic_filter.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CharacteristicsFilter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final itemFilter = Provider.of<ItemFilter>(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 12),
           child: Text(
-            S.of(context).items_characteristics,
+            '${S.of(context).items_characteristics} (${itemFilter.characteristics.length})',
             style: TextStyle(
               fontFamily: 'Lato',
               fontWeight: FontWeight.w400,
